93a6aae232df663e21612a4cd1add364d7c9a658
Jacob Jiskoot <jajiskoot@knights.ucf.edu>
2020-03-21 14:30:49 -0400

    Create React Native Project 'TemplateReactNative' add custom eslint and prettier rules

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  root: true,
+  extends: ["airbnb", "prettier", "prettier/react"],
+  plugins: ["react-hooks", "prettier"],
+  env: {
+    browser: true,
+    jest: true
+  },
+  parser: "babel-eslint",
+  rules: {
+    "no-plusplus": 0,
+    "no-underscore-dangle": 0,
+    "no-unused-vars": 2,
+    "prettier/prettier": [
+      "error",
+      {
+        "bracketSpacing": true,
+        "printWidth": 120,
+        "semi": true,
+        "singleQuote": true,
+        "tabWidth": 4,
+        "trailingComma": "none"
+      }
+    ],
+    "react/jsx-indent": 0,
+    "react/jsx-indent-props": [2, 4]
+  }
+};
